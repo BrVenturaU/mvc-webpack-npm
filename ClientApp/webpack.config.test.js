ae407d54a9ee9831a0e2e401230ce9cec760dd46
BrVenturaU <brandonventura16@gmail.com>
2021-08-20 16:12:37 -0600

    Add extra configuration for more deployments.

---
+++ ClientApp/webpack.config.test.js
@@ -0,0 +1,22 @@
+﻿const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const Dotenv = require('dotenv-webpack');
+const { merge } = require('webpack-merge');
+const commonConfig = require('./webpack.config.js');
+
+module.exports = merge(commonConfig, {
+    devtool: 'source-map',
+    mode: 'production',
+    plugins: [
+        new Dotenv({
+            path:'./app.test.env',
+            systemvars: true
+        })
+    ],
+    optimization: {
+        minimizer: [
+            // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+            // `...`,
+            new CssMinimizerPlugin(),
+        ],
+    },
+});
